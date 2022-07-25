f2b62f67739ee982303e86f7fc288b37e65612fd
Renan Skonicezny Vilela <renansko@gmail.com>
2022-07-24 22:41:50 -0300

    eita

--- velha.cpp
+++ velha.cpp
@@ -5,6 +5,8 @@
 #include <fstream>
 #include <stdio.h>
 #include "seguro.h"
+#include <string>
+#include <cstring>
 
 using namespace std;
 
@@ -13,6 +15,10 @@ char digit[9] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
 //char* password = seguro;
 //char* user = username;
+bool checkEmptyString(const char* s)
+{
+    return strlen(s) == 0;
+}
 
 void bd()
 {
@@ -83,6 +89,11 @@ char* login(void)
     fgets(email, 99, stdin);
     email[strcspn(email, "\n")] = 0;
 
+    if (strlen(password) == 0 || strcmp(password, "0") == 0) {
+        printf("\7\a\nDigite alguem valor!\n");
+        exit(EXIT_FAILURE);
+    }
+
     while ((c = getchar()) != '\n' && c != EOF) {};
 
     
@@ -92,10 +103,13 @@ char* login(void)
     fgets(password, 99, stdin);
     password[strcspn(password, "\n")] = 0;
     while ((c = getchar()) != '\n' && c != EOF) {};
+
+    if (strlen(password) == 0 || strcmp(password, "0") == 0) {
+        printf("\7\a\nDigite alguem valor!\n");
+        exit(EXIT_FAILURE);
+    }
     const char* value = "*";
     const char* table = "USERS";
-
-    
 
     sprintf_s(query, "select * from %s where email = '%s' AND password = '%s'", table, email, password);
 
@@ -154,7 +168,14 @@ int registrar() {
 
     fgets(email, 99, stdin);
     email[strcspn(email, "\n")] = 0;
-	while ((c = getchar()) != '\n' && c != EOF) {};
+    while ((c = getchar()) != '\n' && c != EOF) {};
+  
+    if (strlen(email) == 0 || strcmp(email, "0") == 0) {
+        printf("\7\a\nDigite alguem valor!\n");
+        exit(EXIT_FAILURE);
+    }
+    sprintf_s(query, "SELECT * FROM %s WHERE email = '%s'", table, email);
+
 
     if (mysql_query(conn, query))
     {
@@ -176,6 +197,11 @@ int registrar() {
     cout << "----------------------\n";
     cout << "Digite sua nome:    " << endl;
 
+    if (strlen(nome) == 0 || strcmp(nome, "0") == 0) {
+        printf("\7\a\nDigite alguem valor!\n");
+        exit(EXIT_FAILURE);
+    }
+
     fgets(nome, 99, stdin);
     nome[strcspn(nome, "\n")] = 0;
     while ((c = getchar()) != '\n' && c != EOF) {};
@@ -187,6 +213,11 @@ int registrar() {
 
     fgets(password, 99, stdin);
     password[strcspn(password, "\n")] = 0;
+
+    if (strlen(password) == 0 || strcmp(password, "0") == 0) {
+        printf("\7\a\nDigite alguem valor!\n");
+        exit(EXIT_FAILURE);
+    }
 
     while ((c = getchar()) != '\n' && c != EOF) {};
 
@@ -204,8 +235,9 @@ int registrar() {
         mysql_close(conn);
         exit(0);
     }
-
-    return 1;
+    int acc = 0;
+    acc++;
+    return acc;
 }
 
 //display tic tac toe
@@ -231,9 +263,9 @@ std::pair<std::string, std::string> displayMenu()
     for (int i = 1; i <= acc; i++) {
         cout << "Menu" << endl;
         cout << "----------------------\n";
-        cout << "Login do :" << i << " player digite 1." << endl;
+        cout << "Login do :  " << i << " player digite 1." << endl;
         cout << "----------------------\n";
-        cout << "Registrar o:" << i << " player digite 2." << endl;
+        cout << "Registrar o:  " << i << " player digite 2." << endl;
         cout << "----------------------\n";
         cout << "Sair digite qualquer outro numero." << endl;
 
@@ -308,25 +340,26 @@ public:
 
     }
 
-    void check(int value)
+    void check(int value, std::string player_1, std::string player_2)
     {
   
         if (numbers[value] == 'X')
         {
-            cout << "==> Player 1 wins" << endl;            
+            cout << "==> Player "<< player_1 << " wins" << endl;
+            exit(0);
         }
         else if (numbers[value] == 'O')
         {
           
-            cout << "==> Player 2 wins" << endl;
+            cout << "==> Player "<< player_2 << " wins" << endl;
             exit(0);
         }
 
     }
 
     // checks who won the game
-    void win()
-    {
+    void win(std::string player_1, std::string player_2)
+    { 
         int x = 0;
         for (int i = 0; i < 9; i++)
         {
@@ -335,7 +368,7 @@ public:
             {
                 if (numbers[i] == numbers[i += 1] && numbers[i] == numbers[i += 1])
                 {
-                    check(i);
+                    check(i, player_1, player_2);
                 }
             }
             if (i == 8)
@@ -347,7 +380,7 @@ public:
             {
                 if (numbers[i] == numbers[i += 3] && numbers[i] == numbers[i += 3])
                 {
-                    check(i);
+                    check(i,player_1,player_2);
                 }
                 else
                 {
@@ -370,11 +403,11 @@ public:
         //checks diagonal right and left
         if (numbers[0] == numbers[4] && numbers[0] == numbers[8])
         {
-            check(0);
+            check(0, player_1, player_2);
         }
         else if (numbers[2] == numbers[4] && numbers[2] == numbers[6])
         {
-            check(2);
+            check(2, player_1, player_2);
         }
     }
 };
@@ -390,6 +423,7 @@ int main()
     std::string player_1 = player.first;
     std::string player_2 = player.second;
     std::string temp;
+
     char number;
     Players player1;
     Players player2;
@@ -397,13 +431,14 @@ int main()
 
     cout << "\tTic Tac Toe" << endl;
     cout << player_1  << " 'player 1' (X) - " << player_2 << " 'player 2' (O)" << endl;
-    display();
+    
 
     //switch between players
     for (int n = 0; n < 5; n++)
     {
         for (int i = 1; i < 3; i++)
         {
+            display();
             cout << "Player " << i << ", enter a number: ";
             cin >> number;
             for (int q = 0; q < 9; q++)
@@ -422,7 +457,7 @@ int main()
             if (i == 1)
             {
                 player1.player(number, i);
-                player1.win();
+                player1.win(player_1, player_2);
             }
             if (i == 1 && n == 4)
             {
@@ -432,7 +467,7 @@ int main()
             else if (i == 2)
             {
                 player2.player(number, i);
-                player2.win();
+                player2.win(player_1, player_2);
 
 
             }
